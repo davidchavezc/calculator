a245e9893da33e6595e89fc6b874d1d98327c1bc
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-09-13 21:41:47 -0600

    Add operator functions

---
+++ operations.js
@@ -0,0 +1,18 @@
+function sum(num1, num2){
+  return num1 + num2;
+}
+
+function subtract(num1, num2){
+  return num1 - num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function divide(num1, num2){
+  if (num2 === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return num1 / num2;
+}
